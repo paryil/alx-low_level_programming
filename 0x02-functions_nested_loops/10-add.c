842e4f37bc0150c89ef8ca1c9e725c4542ab0fa8
paryil <mbaiparyilpatrick@gmail.com>
2022-09-14 18:54:50 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two intergers 
+ * @a: two interger arguments
+ * @b: two interger arguments
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a+b);
+}
