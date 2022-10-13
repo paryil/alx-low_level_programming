78ed78b2c78c32e80b8852324724650b81add394
paryil <mbaiparyilpatrick@gmail.com>
2022-10-12 23:50:48 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-	if (array && cmp)
-	{
-			for (i = 0; i < size; i++)
-			{
-			if (cmp(array[i]) != 0)
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
 
-			return (i);
-			}
-	}
-
-	return (-1);
+return (-1);
 }
